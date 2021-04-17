40d5d7253c8f7d37f3623f6864d687791d17380f
Alish Dhamala <35582736+alish1129@users.noreply.github.com>
2021-04-17 05:39:46 -0500

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as LoginModal } from "./LoginModal/LoginModal";
+export { default as VideoContainer } from "./VideoContainer/VideoContainer";
+export { default as Room } from "./Room/Room";
